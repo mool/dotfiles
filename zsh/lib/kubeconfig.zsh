8a8f8a5f1e8329f7801316ec4fabd6f5d7559cec
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2020-10-23 15:47:44 -0300

    update the config for kubectl

--- zsh/lib/kubeconfig.zsh
+++ zsh/lib/kubeconfig.zsh
@@ -1,19 +1,5 @@
 alias k=kubectl
 
-export KUBECONFIG=$HOME/.kube/config
-
 INV_REPOS_PATH=$HOME/InVision
-INVISION_KUBECONFIG_PATH=$INV_REPOS_PATH/cluster-provisioning/kubeconfig
-PLATFORM_ONE_PATH=$INV_REPOS_PATH/platform-one-infra
-CLUSTERS=(
-  ops-221185518522
-  ops-test-556453037222
-)
-
-for f in $(ls $INVISION_KUBECONFIG_PATH); do
-  export KUBECONFIG=$KUBECONFIG:$INVISION_KUBECONFIG_PATH/$f
-done
-
-for cluster in "${CLUSTERS[@]}"; do
-  export KUBECONFIG=$KUBECONFIG:$PLATFORM_ONE_PATH/$cluster/kubeconfig
-done
+export INVISION_KUBECTL_CONFIG_PATH=$INV_REPOS_PATH/kubectl-config
+source $INVISION_KUBECTL_CONFIG_PATH/kubectl.sh
