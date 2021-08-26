89048931a7eb0278f40eaad41115c1f4b26b3c9d
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2021-08-26 12:26:09 -0300

    script with OSX settings

---
+++ osx/settings.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+# Disable natural scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Screenshots
+SCREENSHOTS_PATH=${HOME}/Desktop/screenshots
+mkdir -p ${SCREENSHOTS_PATH}
+defaults write com.apple.screencapture location -string "${SCREENSHOTS_PATH}"
+
+# Chrome Settings
+defaults write com.google.Chrome ExternalProtocolDialogShowAlwaysOpenCheckbox -bool true
