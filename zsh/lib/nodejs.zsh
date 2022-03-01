1392715b7fe6eaf85e8eeb9aee6279e7e1d25e87
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2022-03-01 16:59:08 -0300

    add nodejs config

---
+++ zsh/lib/nodejs.zsh
@@ -0,0 +1,5 @@
+export PATH="/usr/local/opt/node@16/bin:$PATH"
+
+#For compilers to find node@16 you may need to set:
+export LDFLAGS="-L/usr/local/opt/node@16/lib"
+export CPPFLAGS="-I/usr/local/opt/node@16/include"
