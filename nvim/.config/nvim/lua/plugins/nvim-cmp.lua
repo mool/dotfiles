e1a1241f9982ef8499e36386f16dbda0e386b168
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2024-12-16 15:20:26 -0300

    update comment

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -36,8 +36,7 @@ return {
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
-				-- Copilot Source
-				{ name = "copilot" },
+				{ name = "copilot" }, -- Copilot Source
 				{ name = "nvim_lsp" }, -- lsp
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
