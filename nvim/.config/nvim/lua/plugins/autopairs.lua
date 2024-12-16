43d866526acf7afccdd9bbc96b1a0ef4352fb7be
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2024-12-16 15:21:11 -0300

    fix format

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,10 +1,10 @@
 return {
-  'windwp/nvim-autopairs',
-  event = "InsertEnter",
-  opts = {
-    check_ts = true, -- enable treesitter
-    ts_config = {
-      lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-    },
-  }
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	opts = {
+		check_ts = true, -- enable treesitter
+		ts_config = {
+			lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+		},
+	},
 }
