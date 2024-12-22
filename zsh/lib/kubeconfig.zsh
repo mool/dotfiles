464e027bfb4c5a1ed92abbd69e5d5395969f18fa
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2024-12-22 20:22:32 -0300

    configure kubecolor

--- zsh/lib/kubeconfig.zsh
+++ zsh/lib/kubeconfig.zsh
@@ -3,4 +3,5 @@ source <(kubectl completion zsh)
 export PATH="${PATH}:${HOME}/.krew/bin"
 export KUBECONFIG=$HOME/.kube/config:$HOME/.kube/config.mool
 
-alias k=kubectl
+alias k=kubecolor
+compdef kubecolor=kubectl
