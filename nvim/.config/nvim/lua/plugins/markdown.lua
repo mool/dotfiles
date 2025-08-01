5a991352a56ce2cada22d33b6e85fb9e3bdc1bee
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2025-08-01 11:57:09 -0300

    move pencil config

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -2,9 +2,6 @@ return {
 	{
 		"tadmccorkle/markdown.nvim",
 		ft = "markdown", -- or 'event = "VeryLazy"'
-		opts = {
-			-- configuration here or empty for defaults
-		},
 	},
 	{
 		"iamcco/markdown-preview.nvim",
@@ -12,6 +9,23 @@ return {
 		ft = { "markdown" },
 		build = function()
 			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	{
+		"preservim/vim-pencil",
+		ft = "markdown",
+		opt = false,
+		config = function()
+			vim.g["pencil#textwidth"] = 80
+			vim.g["pencil#wrapModeDefault"] = "hard"
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "markdown", -- applied to markdown files
+				callback = function()
+					vim.opt_local.spell = true -- turn on spellcheck
+					vim.cmd(":PencilHard") -- enable hard wrapping
+				end,
+			})
 		end,
 	},
 }
