e3ca405937807848d1820643b6adff2e2cd63e69
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2025-08-01 12:00:07 -0300

    update keymaps and options

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,12 +7,14 @@ local keymap = vim.keymap -- for conciseness
 -- General Keymaps
 ---------------------
 
-keymap.set("n", "<C-d>", "<C-d>zz", {noremap = true})
-keymap.set("n", "<C-u>", "<C-u>zz", {noremap = true})
-keymap.set("n", "n", "nzzzv", {noremap = true})
-keymap.set("n", "N", "Nzzzv", {noremap = true})
+-- set the cursor in the center of the screen when moving
+keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
+keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
+keymap.set("n", "n", "nzzzv", { noremap = true })
+keymap.set("n", "N", "Nzzzv", { noremap = true })
 
-keymap.set("x", "<leader>p", "\"_dP", {noremap = true})
+-- it pastes over a visual selection without overwriting the yank register
+keymap.set("x", "<leader>p", '"_dP', { noremap = true })
 
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>")
@@ -31,16 +33,3 @@ keymap.set("n", "<leader>-", "<C-x>") -- decrement
 -- keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 -- keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 -- keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
--- keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
-
--- keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
--- keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
--- keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
--- keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
-
-----------------------
--- Plugin Keybinds
-----------------------
-
--- vim-maximizer
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
