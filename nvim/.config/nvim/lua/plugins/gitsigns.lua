9f2d40836b4bc6e5a60bddbd8f735bc651af7913
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2025-08-01 12:01:09 -0300

    update gitsign config

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,5 +1,8 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = true,
+	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+
+	config = function()
+		require("gitsigns").setup({})
+	end,
 }
