c0107aba332e6f2a48cf6a5f69ca184c38cffea9
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2025-12-15 16:56:28 -0300

    update markdown config

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,7 +1,12 @@
 return {
 	{
-		"tadmccorkle/markdown.nvim",
-		ft = "markdown", -- or 'event = "VeryLazy"'
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
 	},
 	{
 		"iamcco/markdown-preview.nvim",
@@ -11,21 +16,21 @@ return {
 			vim.fn["mkdp#util#install"]()
 		end,
 	},
-	{
-		"preservim/vim-pencil",
-		ft = "markdown",
-		opt = false,
-		config = function()
-			vim.g["pencil#textwidth"] = 80
-			vim.g["pencil#wrapModeDefault"] = "soft"
-
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "markdown", -- applied to markdown files
-				callback = function()
-					vim.opt_local.spell = true -- turn on spellcheck
-					vim.cmd(":PencilSoft") -- enable hard wrapping
-				end,
-			})
-		end,
-	},
+	-- {
+	-- 	"preservim/vim-pencil",
+	-- 	ft = "markdown",
+	-- 	opt = false,
+	-- 	config = function()
+	-- 		vim.g["pencil#textwidth"] = 80
+	-- 		vim.g["pencil#wrapModeDefault"] = "soft"
+	--
+	-- 		vim.api.nvim_create_autocmd("FileType", {
+	-- 			pattern = "markdown", -- applied to markdown files
+	-- 			callback = function()
+	-- 				vim.opt_local.spell = true -- turn on spellcheck
+	-- 				vim.cmd(":PencilSoft") -- enable hard wrapping
+	-- 			end,
+	-- 		})
+	-- 	end,
+	-- },
 }
