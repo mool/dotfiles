1589f6d893bdd82970b0d47751d3185f2e9031ce
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2026-02-24 16:32:58 -0300

    remove KUBECONFIG

--- zsh/lib/kubeconfig.zsh
+++ zsh/lib/kubeconfig.zsh
@@ -1,7 +1,6 @@
 source <(kubectl completion zsh)
 
 export PATH="${PATH}:${HOME}/.krew/bin"
-export KUBECONFIG=$HOME/.kube/config:$HOME/.kube/config.mool
 
 alias k=kubecolor
 compdef kubecolor=kubectl
