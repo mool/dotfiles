a909c0d98f9305aa3b8d3ecd6274a669718ab411
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2026-05-01 11:24:01 -0300

    add path for psql, export openbao vars

--- zsh/lib/interval.zsh
+++ zsh/lib/interval.zsh
@@ -1,5 +1,12 @@
 export PULUMI_PARALLEL=45
 
+export BAO_ADDR="https://openbao.ops-oma-prod.interval.team"
+export VAULT_ADDR="$BAO_ADDR"
+export VAULT_SERVER_URL="$BAO_ADDR"
+
+# Add psql path
+export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+
 function gcp-login() {
   gcloud auth login
   gcloud auth application-default login
